110fbc3dbc71d3bb0f6a51c63bdaefab96b1f0ca
elenabaurkot <elenabaurkot@gmail.com>
2020-02-05 23:13:49 -0500

    storage started

--- Develop/index.js
+++ Develop/index.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
-  // Questions: If I set a value for each hour and I try to call the value like this console.log($("#time-9").val); it doesn't work, comes up blank or undefined, works when I try on calculator
-  // Question: How do I make the text showing what hour on the planner be more responsive to stay inside its column?
-
   // Global variables
   var currentHour = moment().hour();
-  console.log(currentHour);
-  console.log(typeof currentHour);
   var planningHourValue = $(".planningHour");
-  console.log(planningHourValue);
+  var savedEvents = [];
+
+  // call function to show saved planner events from local storage
+  init();
 
   // today's date shown at the top of the page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-  // on-click function for save button
-  $(".saveBtn").on("click", function() {});
 
-  // function to initialize dayplanner with correct colorcoding based on the hour
-  // this is where I wanted to use value not class, but couldn't get it to work
+  // on-click function for save button to store what is entered to the planner
+  $(".saveBtn").on("click", function() {
+    event.preventDefault();
+    var plannerText = $("#textarea").value; 
+    // Return from function early if submitted todoText is blank
+    if (plannerText === "") {
+      return;
+    }
+    //push new event into savedEvents array
+      savedEvents.push({plannerEvents:plannerText});
+      // store updated events in local storage
+      localStorage.setItem("plannerEvents", JSON.stringify(savedEvents));
+  });
 
-  console.log("planningHourValue.length", planningHourValue.length);
+  
+  // get day planner events from local storage
+  // parson JSON string to an object
+  function init() {
+    var getEvents = JSON.parse(localStorage.getItem("plannerEvents"));
+    // If events were retrieved from localStorage, update the savedEvents array
+    if (getEvents !== null) {
+      savedEvents = getEvents;
+    }
+  }
+
+  // initialize dayplanner with correct colorcoding based on the hour
   for (var i = 0; i < planningHourValue.length; i++) {
     console.log("planningHourValue:", planningHourValue[i]);
     console.log(
@@ -27,19 +45,16 @@ $(document).ready(function() {
     );
     var hourValue = parseInt(planningHourValue[i].getAttribute("data-value"));
     if (currentHour > hourValue) {
-      //   $(".planningHour").addClass("past");
       var pastValue = planningHourValue[i].setAttribute(
         "class",
         "col-10 planningHour past"
       );
     } else if (currentHour < hourValue) {
-      //   $(".planningHour").addClass("future");
       var futureValue = planningHourValue[i].setAttribute(
         "class",
         "col-10 planningHour future"
       );
     } else if (currentHour === hourValue) {
-      //   $(".planningHour").addClass("present");
       var presentValue = planningHourValue[i].setAttribute(
         "class",
         "col-10 planningHour present"
@@ -47,4 +62,3 @@ $(document).ready(function() {
     }
   }
 });
-
