1d66f4b038722416b9ca0edf1fd6806f46fcade0
elenabaurkot <elenabaurkot@gmail.com>
2020-02-18 14:38:26 -0500

    storage working properly

--- Develop/index.js
+++ Develop/index.js
@@ -2,30 +2,38 @@ $(document).ready(function() {
   // Global variables
   var currentHour = moment().hour();
   var planningHourValue = $(".planningHour");
-  var savedEvents = [];
+  var savedEvents = {
+    9: "",
+    10: "",
+    11: "", 
+    12: "",
+    13: "",
+    14: "",
+    15: "", 
+    16: "",
+    17: ""
+  }
 
   // call function to show saved planner events from local storage
   init();
 
-
   // today's date shown at the top of the page
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-
-  // planner events persist when local storage is cleared or when edited, not sure how to make it so they go away if planner page is blank--> think you should try splicing
   // On click function for save button
   $(".saveBtn").on("click", function(event) {
-    var index = $(this).attr("value");
+    console.log(event.currentTarget.id)
+    var index = event.currentTarget.id;
     var plannerText = $("#text-" + index).val();
     //saving events to local storage
     console.log("index: " + index + "value: " + plannerText);
-    savedEvents.push({ indexHour: index, textValue: plannerText });
+    savedEvents[index] = plannerText;
+    console.log(savedEvents);
     // store updated events in local storage
     localStorage.setItem("plannerEvents", JSON.stringify(savedEvents));
   });
 
 
-  
   // get day planner events from local storage
   // parson JSON string to an object
   function init() {
@@ -33,14 +41,19 @@ $(document).ready(function() {
     // If events were retrieved from localStorage, update the savedEvents array
     if (getEvents !== null) {
       savedEvents = getEvents;
+    } else {
+      return;
     }
-    // push saved events back onto page by appending if the value of the text area is equal to the index hour of the array object
-    var textAreaValue = $("textarea").attr("value");
-    $.each(savedEvents, function(i, item) {
-      $("#text-" + item.indexHour).append(item.textValue);
-    });
-  }
+    // push saved events back onto page by appending if the key is equal to the index hour of the array object
+    var keys = Object.entries(getEvents)
 
+    keys.forEach(([key, value]) => {
+      for (var i = key; i < 18; i++)
+      if ($("#text-"+ i).attr('data-time') === key) {
+        $('#text-'+ i).append(value);
+      }
+    })
+  }
 
   // initialize dayplanner with correct colorcoding based on the hour
   for (var i = 0; i < planningHourValue.length; i++) {
